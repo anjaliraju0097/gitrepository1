1bff4cb7d41afdc2f68354cebd82a80d676a734b
anjaliraju0097 <77439213+anjaliraju0097@users.noreply.github.com>
2024-10-15 17:23:05 +0530

    Update ssh_action.sh

--- ssh_action.sh
+++ ssh_action.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+echo "Starting script execution..."
+echo "Listing files in the current directory:"
+ls -la
+echo "Current date and time:"
+date
+if [ -f "./requirement.txt" ]; then
+    echo "Found requirement.txt"
+else
+    echo "requirement.txt not found."
+fi
+
+echo "Script execution finished."
 
